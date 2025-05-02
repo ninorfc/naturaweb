409675c92e79844248e6e3ad3d6be1071ee59d32
Rafael Francisco de Camargo <ninocamargorama@gmail.com>
2025-05-02 07:32:34 -0300

    Versão com estrutura Next.js completa e dependencias

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-green-50 text-green-900">
+      <h1 className="text-4xl font-bold">Bem-vindo ao Naturaweb 🌿</h1>
+    </main>
+  );
+}
